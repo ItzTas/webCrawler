8201d955fd744ea963578a469801ee791b1f67ad
ItzTas <talessabini2@gmail.com>
2024-04-24 16:15:42 -0300

    I created a function to administrate the comands

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-function main() {}
+function processArguments() {
+  if (process.argv.length < 3) {
+    console.log("The arguments cannot be less than 1");
+  } else if (process.argv.length > 3) {
+    console.log("The arguments cannot be greater than 1");
+  } else {
+    console.log(
+      `The crawler is starting with the ${process.argv[2]} as the base url`
+    );
+  }
+}
+
+function main() {
+  processArguments();
+}
+
+main();
